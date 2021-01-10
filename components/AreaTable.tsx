c539baff4cc4a44a27c156f0d2a9c32ee8c0bec2
Tran Dai Dung <trandaidung2810@gmail.com>
2021-01-10 22:09:22 +0700

    cosmetic

--- components/AreaTable.tsx
+++ components/AreaTable.tsx
@@ -1,12 +1,36 @@
 import { TableBuilder, TableBuilderColumn } from 'baseui/table-semantic'
 import Link from "next/link"
 import { StyledLink } from 'baseui/link'
+import { useStyletron } from 'baseui'
 
-const AreaNameCell = ({ id, name }) => (
-  <Link href={'/' + id} passHref>
-    <StyledLink>{name}</StyledLink>
-  </Link>
-)
+const UNITS = {
+  municipality: 'city', // 'Thành phố Trung ương',
+  province: 'province', // 'Tỉnh',
+  municipal_city: 'city', // 'Thành phố trực thuộc Trung ương',
+  provincial_city: 'city', // 'Thành phố',
+  urban_district: 'district', // 'Quận',
+  town: 'town', // 'Thị xã',
+  district: 'district', // 'Huyện',
+  ward: 'ward', // 'Phường',
+  township: 'township', // 'Thị trấn',
+  commune: 'commune', // 'Xã'
+}
+
+const AreaNameCell = ({ id, name, unit }) => {
+  const [css] = useStyletron()
+  return (
+    <Link href={'/' + id} passHref>
+      <StyledLink className={css({
+        textTransform: 'capitalize'
+      })}>
+        {Number(name)
+          ? `${UNITS[unit]} ${name}`
+          : `${name} ${UNITS[unit]}`
+        }
+      </StyledLink>
+    </Link>
+  )
+}
 
 export default function AreaTable({ loading, data }) {
   return (
@@ -21,11 +45,11 @@ export default function AreaTable({ loading, data }) {
         {row => row.code}
       </TableBuilderColumn>
       <TableBuilderColumn header='Name'>
-        {({ id, name }) => <AreaNameCell id={id} name={name} />}
+        {({ id, name, unit }) => <AreaNameCell id={id} name={name} unit={unit} />}
       </TableBuilderColumn>
-      <TableBuilderColumn header='Unit'>
+      {/* <TableBuilderColumn header='Unit'>
         {row => row.unit}
-      </TableBuilderColumn>
+      </TableBuilderColumn> */}
     </TableBuilder>
   )
 }
