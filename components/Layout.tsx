ac73cf6b827720a112c3465856784d07d4454ef0
Tran Dai Dung <trandaidung2810@gmail.com>
2021-01-10 14:22:13 +0700

    update Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,20 @@
+import Header from './Header'
+import { Block } from 'baseui/block'
+import { useStyletron } from 'baseui'
+
+const Layout = props => {
+  const [css, theme] = useStyletron()
+  const { scale600, scale800 } = theme.sizing
+  return (
+    <>
+      <Header />
+      <Block
+        padding={[scale600, scale600, scale800]}
+      >
+        {props.children}
+      </Block>
+    </>
+  )
+}
+
+export default Layout
