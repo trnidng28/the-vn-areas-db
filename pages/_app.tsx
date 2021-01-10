1d7e91cf96fdfe570e36c109e9b0542f3daeb0aa
Tran Dai Dung <trandaidung2810@gmail.com>
2021-01-10 14:11:30 +0700

    add style.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import { Provider as StyletronProvider } from 'styletron-react'
 import { styletron } from '~/lib/styletron'
 import { BaseProvider, LightTheme } from 'baseui'
+import '../style.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
