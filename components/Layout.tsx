dc5c72cc760a0a433e930531436b5fab423f2084
Tran Dai Dung <trandaidung2810@gmail.com>
2021-01-16 21:51:10 +0700

    add title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,12 +1,16 @@
 import Header from './Header'
 import { Block } from 'baseui/block'
 import { useStyletron } from 'baseui'
+import Head from 'next/head'
 
 const Layout = props => {
   const [_, theme] = useStyletron()
   const { scale800 } = theme.sizing
   return (
     <>
+      <Head>
+        <title>The Viet Nam's Areas DB</title>
+      </Head>
       <Header />
       <Block
         padding={[scale800]}
